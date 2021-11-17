b66dc44afade23df7008fc11ff60988697a6fbbf
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-17 23:23:12 +0900

    add model respondents.go

---
+++ domain/model/respondents.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"gopkg.in/guregu/null.v4"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Respondents struct {
+	ResponseID      int            `json:"responseID" gorm:"column:response_id;type:int(11) AUTO_INCREMENT;not null;primaryKey"`
+	QuestionnaireID int            `json:"questionnaireID" gorm:"type:int(11);not null"`
+	UserTraqid      string         `json:"user_traq_id,omitempty" gorm:"type:varchar(32);size:32;default:NULL"`
+	ModifiedAt      time.Time      `json:"modified_at,omitempty" gorm:"type:DATETIME;not null;default:CURRENT_TIMESTAMP"`
+	SubmittedAt     null.Time      `json:"submitted_at,omitempty" gorm:"type:DATETIME NULL;default:NULL"`
+	DeletedAt       gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"type:DATETIME NULL;default:NULL"`
+	Responses       []Responses    `json:"-" gorm:"foreignKey:ResponseID;references:ResponseID"`
+}
