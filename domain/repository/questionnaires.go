a7beea7202b7c155776bb5062825d92dc01d18fc
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-18 03:06:45 +0900

    :recycle:

--- domain/repository/questionnaires.go
+++ domain/repository/questionnaires.go
@@ -1,22 +1,21 @@
 package repository
 
 import (
+	"context"
 	"github.com/xxarupkaxx/anke-two/domain/model"
-	"github.com/xxarupkaxx/anke-two/usecase/input"
-	"github.com/xxarupkaxx/anke-two/usecase/output"
 	"gopkg.in/guregu/null.v4"
 )
 
 type IQuestionnaire interface {
-	InsertQuestionnaire(questionnaires input.Questionnaires) (int, error)
-	UpdateQuestionnaire(questionnaires input.Questionnaires) error
-	DeleteQuestionnaire(questionnaires input.Questionnaires) error
-	GetQuestionnaires(questionnaires input.Questionnaires) ([]output.QuestionnaireInfo, error)
-	GetAdminQuestionnaires(questionnaires input.Questionnaires) ([]model.Questionnaires, error)
-	GetQuestionnaireInfo(questionnaires input.Questionnaires) (output.QuestionnaireInfo, error)
-	GetTargetedQuestionnaires(questionnaires input.Questionnaires) ([]output.TargetedQuestionnaire, error)
-	GetQuestionnaireLimit(questionnaires input.Questionnaires) (null.Time, error)
-	GetQuestionnaireLimitByResponseID(questionnaires input.Questionnaires)(null.Time,error)
-	GetResponseReadPrivilegeInfoByResponseID(questionnaires input.Questionnaires) (*output.ResponseReadPrivilegeInfo, error)
-	GetResponseReadPrivilegeInfoByQuestionnaireID(questionnaires input.Questionnaires) (*output.ResponseReadPrivilegeInfo, error)
+	InsertQuestionnaire(ctx context.Context, title string, description string, resTimeLimit null.Time, resSharedTo string) (int, error)
+	UpdateQuestionnaire(ctx context.Context, title string, description string, resTimeLimit null.Time, resSharedTo string, questionnaireID int) error
+	DeleteQuestionnaire(ctx context.Context, questionnaireID int) error
+	GetQuestionnaires(ctx context.Context, userID string, sort string, search string, pageNum int, nontargeted bool) ([]model.QuestionnaireInfo, error)
+	GetAdminQuestionnaires(ctx context.Context, userID string) ([]model.Questionnaires, error)
+	GetQuestionnaireInfo(ctx context.Context, questionnaireID int) (model.QuestionnaireInfo, error)
+	GetTargetedQuestionnaires(ctx context.Context, userID string, answered string, sort string) ([]model.TargetedQuestionnaire, error)
+	GetQuestionnaireLimit(ctx context.Context, questionnaireID int) (null.Time, error)
+	GetQuestionnaireLimitByResponseID(ctx context.Context, responseID int)(null.Time,error)
+	GetResponseReadPrivilegeInfoByResponseID(ctx context.Context, userID string, responseID int) (*model.ResponseReadPrivilegeInfo, error)
+	GetResponseReadPrivilegeInfoByQuestionnaireID(ctx context.Context, userID string, questionnaireID int) (*model.ResponseReadPrivilegeInfo, error)
 }
