b97205ea8e1d0b71800e604d2d5bd92a53eec45f
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-17 23:23:00 +0900

    add model responses.go

---
+++ domain/model/responses.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"gopkg.in/guregu/null.v4"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Responses struct {
+	ResponseID int            `json:"-" gorm:"type:int(11);not null;primaryKey"`
+	QuestionID int            `json:"-" gorm:"type:int(11);not null;primaryKey"`
+	Body       null.String    `json:"response" gorm:"type:text;default:NULL"`
+	ModifiedAt time.Time      `json:"-" gorm:"type:DATETIME;not null;default:CURRENT_TIMESTAMP"`
+	DeletedAt  gorm.DeletedAt `json:"-" gorm:"type:DATETIME NULL;default:NULL"`
+}
