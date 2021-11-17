7b3f18c83e420fd9db2ca66a8c9c75d1eaafceb6
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-17 19:25:09 +0900

    client

---
+++ client/src/pages/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>404 Not Found</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+  data() {
+    return {
+      title: 'ページが見つかりませんでした'
+    }
+  }
+}
+</script>
