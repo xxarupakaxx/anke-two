4d6a3f2b5b3452d025d903ac47c2481cc1aadad8
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-18 01:23:43 +0900

    add model scale_labels.go

---
+++ domain/model/scale_labels.go
@@ -0,0 +1,9 @@
+package model
+
+type ScaleLabels struct {
+	QuestionID      int    `json:"questionID" gorm:"type:int(11) AUTO_INCREMENT;not null;primaryKey"`
+	ScaleLabelRight string `json:"scale_label_right" gorm:"type:text;default:NULL;"`
+	ScaleLabelLeft  string `json:"scale_label_left" gorm:"type:text;default:NULL;"`
+	ScaleMin        int    `json:"scale_min" gorm:"type:int(11);default:NULL;"`
+	ScaleMax        int    `json:"scale_max" gorm:"type:int(11);default:NULL;"`
+}
