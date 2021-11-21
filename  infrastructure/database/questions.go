170ce336c7b00313f3ea4919a69ba76a8e6bb0aa
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-22 03:09:27 +0900

    コンストラクタ

---  infrastructure/database/questions.go
+++  infrastructure/database/questions.go
@@ -6,11 +6,20 @@ import (
 	infrastructure "github.com/xxarupkaxx/anke-two/ infrastructure"
 	"github.com/xxarupkaxx/anke-two/domain/model"
 	"gorm.io/gorm"
+	"log"
 )
 
 type Question struct {
 	//TODO:後で考える
 	infrastructure.SqlHandler
+}
+
+func NewQuestion(sqlHandler infrastructure.SqlHandler) *Question {
+	err := setUpQuestionTypes(sqlHandler.Db)
+	if err != nil {
+		log.Fatalf("failed to get db: %w", err)
+	}
+	return &Question{SqlHandler: sqlHandler}
 }
 
 func setUpQuestionTypes(db *gorm.DB) error {
@@ -57,7 +66,21 @@ func setUpQuestionTypes(db *gorm.DB) error {
 	return nil
 }
 func (q *Question) InsertQuestion(ctx context.Context, questionnaireID int, pageNum int, questionNum int, questionType string, body string, isRequired bool) (int, error) {
+	db, err := GetTx(ctx)
+	if err != nil {
+		return 0, fmt.Errorf("failed to get transaction:%w", err)
+	}
+	var intQuestionType int
+	var questionsType model.QuestionType
+	err = db.
+		Where("question_type = ?", questionType).
+		Select("id").
+		First(&questionsType).Error
+	if err != nil {
+		return 0, fmt.Errorf("failed to get questionType :%w", err)
+	}
 
+	intQuestionType = questionsType.ID
 }
 
 func (q *Question) UpdateQuestion(ctx context.Context, questionnaireID int, pageNum int, questionNum int, questionType string, body string, isRequired bool, questionID int) error {
