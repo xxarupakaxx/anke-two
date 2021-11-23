2fa39639fb1c82e0f4032a9ad43c252d6fd56429
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-23 23:07:17 +0900

    middlewareのインターフェイス化

---
+++ domain/repository/middleware/middleware.go
@@ -0,0 +1,15 @@
+package middleware
+
+import "github.com/labstack/echo/v4"
+
+type IMiddleware interface {
+	SetValidatorMiddleware(next echo.HandlerFunc) echo.HandlerFunc
+	SetUserIDMiddleware(next echo.HandlerFunc) echo.HandlerFunc
+	TraPMemberAuthenticate(next echo.HandlerFunc) echo.HandlerFunc
+	TrapReteLimitMiddlewareFunc() echo.MiddlewareFunc
+	QuestionnaireAdministratorAuthenticate(next echo.HandlerFunc) echo.HandlerFunc
+	ResponseReadAuthenticate(next echo.HandlerFunc) echo.HandlerFunc
+	RespondentsAuthenticate(next echo.HandlerFunc) echo.HandlerFunc
+	QuestionAdministratorAuthenticate(next echo.HandlerFunc) echo.HandlerFunc
+	ResultAuthenticate(next echo.HandlerFunc) echo.HandlerFunc
+}
