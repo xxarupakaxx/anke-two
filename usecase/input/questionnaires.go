f4d4c48c543ae60b548b49f66dd11d443fe7f4a0
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-23 23:58:17 +0900

    PostQuestionRequestの追加

--- usecase/input/questionnaires.go
+++ usecase/input/questionnaires.go
@@ -51,3 +51,20 @@ type QuestionInfo struct {
 	MinBound        string   `json:"min_bound" validate:"omitempty,number"`
 	MaxBound        string   `json:"max_bound" validate:"omitempty,number"`
 }
+
+type PostQuestionRequest struct {
+	QuestionID int      `json:"questionnaireID" validate:"min=0"`
+	QuestionType    string   `json:"question_type" validate:"required,oneof=Text TextArea Number MultipleChoice Checkbox LinearScale"`
+	QuestionNum     int      `json:"question_num" validate:"min=0"`
+	PageNum         int      `json:"page_num" validate:"min=0"`
+	Body            string   `json:"body" validate:"required"`
+	IsRequired      bool     `json:"is_required"`
+	Options         []string `json:"options" validate:"required_if=QuestionType Checkbox,required_if=QuestionType MultipleChoice,dive,max=50"`
+	ScaleLabelRight string   `json:"scale_label_right" validate:"max=50"`
+	ScaleLabelLeft  string   `json:"scale_label_left" validate:"max=50"`
+	ScaleMin        int      `json:"scale_min"`
+	ScaleMax        int      `json:"scale_max" validate:"gtecsfield=ScaleMin"`
+	RegexPattern    string   `json:"regex_pattern"`
+	MinBound        string   `json:"min_bound" validate:"omitempty,number"`
+	MaxBound        string   `json:"max_bound" validate:"omitempty,number"`
+}
