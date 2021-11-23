ab792e3dcf991cb0ef5b0829345b2f1aa19e2ba7
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-24 00:09:52 +0900

    output,inputの追加

--- usecase/input/questionnaires.go
+++ usecase/input/questionnaires.go
@@ -42,6 +42,7 @@ type QuestionInfo struct {
 	PageNum         int      `json:"page_num" validate:"min=0"`
 	Body            string   `json:"body" validate:"required"`
 	IsRequired      bool     `json:"is_required"`
+	CreatedAt       string   `json:"created_at"`
 	Options         []string `json:"options" validate:"required_if=QuestionType Checkbox,required_if=QuestionType MultipleChoice,dive,max=50"`
 	ScaleLabelRight string   `json:"scale_label_right" validate:"max=50"`
 	ScaleLabelLeft  string   `json:"scale_label_left" validate:"max=50"`
@@ -51,7 +52,6 @@ type QuestionInfo struct {
 	MinBound        string   `json:"min_bound" validate:"omitempty,number"`
 	MaxBound        string   `json:"max_bound" validate:"omitempty,number"`
 }
-
 
 type PostQuestionRequest struct {
 	QuestionnaireID int      `json:"questionnaireID" validate:"min=0"`
