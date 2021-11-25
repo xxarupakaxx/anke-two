e86e6bf55cf1fae33625ce3bb7fd78642a47fe75
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-25 23:36:06 +0900

    GetQuestionsの実装

--- usecase/questionnaires.go
+++ usecase/questionnaires.go
@@ -10,7 +10,6 @@ import (
 	"github.com/xxarupkaxx/anke-two/usecase/input"
 	"github.com/xxarupkaxx/anke-two/usecase/output"
 	"gorm.io/gorm"
-	"net/http"
 	"time"
 )
 
@@ -38,7 +37,7 @@ type QuestionnaireUsecase interface {
 	PostQuestionByQuestionnaireID(ctx context.Context, request input.PostQuestionRequest) (output.PostQuestionRequest, error)
 	EditQuestionnaire(ctx context.Context, request input.PostAndEditQuestionnaireRequest) error
 	DeleteQuestionnaire(ctx context.Context) error
-	GetQuestions(ctx context.Context, info input.QuestionInfo) (output.QuestionInfo, error)
+	GetQuestions(ctx context.Context, info input.QuestionInfo) ([]output.QuestionInfo, error)
 }
 
 func (q *questionnaire) POSTQuestionnaire(ctx context.Context, input input.PostAndEditQuestionnaireRequest) (output.PostAndEditQuestionnaireRequest, error) {
@@ -125,40 +124,33 @@ func (q *questionnaire) DeleteQuestionnaire(ctx context.Context) error {
 	panic("implement me")
 }
 
-func (q *questionnaire) GetQuestions(ctx context.Context, info input.QuestionInfo) (output.QuestionInfo, error) {
+func (q *questionnaire) GetQuestions(ctx context.Context, info input.QuestionInfo) ([]output.QuestionInfo, error) {
 	allQuestions, err := q.IQuestion.GetQuestions(ctx, info.QuestionnaireID)
 	if err != nil {
-		return output.QuestionInfo{StatusCode: http.StatusInternalServerError}, err
+		return nil, err
 	}
 	if len(allQuestions) == 0 {
-		return output.QuestionInfo{StatusCode: http.StatusNotFound}, nil
+		return nil, model.ErrRecordNotFound
 	}
 
 	optionIDs := []int{}
 	scaleLabelIDs := []int{}
 	validationIDs := []int{}
-	var questionsType map[int]model.QuestionType
-	for _, question := range allQuestions {
-		questionsType, err = q.IQuestion.ChangeStrQuestionType(ctx, question.QuestionnaireID)
-		if err != nil {
-			return output.QuestionInfo{StatusCode: http.StatusInternalServerError}, err
-		}
-	}
 
-	for questionID, questionType := range questionsType {
-		switch questionType.QuestionType {
+	for _, question := range allQuestions {
+		switch question.Type {
 		case "MultipleChoice", "Checkbox", "Dropdown":
-			optionIDs = append(optionIDs, questionID)
+			optionIDs = append(optionIDs, question.ID)
 		case "LinearScale":
-			scaleLabelIDs = append(scaleLabelIDs, questionID)
+			scaleLabelIDs = append(scaleLabelIDs, question.ID)
 		case "Text", "Number":
-			validationIDs = append(validationIDs, questionID)
+			validationIDs = append(validationIDs, question.ID)
 		}
 	}
 
 	options, err := q.IOption.GetOptions(ctx, optionIDs)
 	if err != nil {
-		return output.QuestionInfo{StatusCode: http.StatusInternalServerError}, err
+		return nil, err
 	}
 	optionMap := make(map[int][]string, len(options))
 	for _, option := range options {
@@ -167,7 +159,7 @@ func (q *questionnaire) GetQuestions(ctx context.Context, info input.QuestionInf
 
 	scaleLabels, err := q.IScaleLabel.GetScaleLabels(ctx, scaleLabelIDs)
 	if err != nil {
-		return output.QuestionInfo{StatusCode: http.StatusInternalServerError}, err
+		return nil, err
 	}
 	scaleLabelMap := make(map[int]model.ScaleLabels, len(scaleLabels))
 	for _, label := range scaleLabels {
@@ -176,11 +168,59 @@ func (q *questionnaire) GetQuestions(ctx context.Context, info input.QuestionInf
 
 	validations, err := q.IValidation.GetValidations(ctx, validationIDs)
 	if err != nil {
-		return output.QuestionInfo{StatusCode: http.StatusInternalServerError}, err
+		return nil, err
 	}
 	validationMap := make(map[int]model.Validations, len(validations))
 	for _, validation := range validations {
 		validationMap[validation.QuestionID] = validation
 	}
+
+	var outputs []output.QuestionInfo
+
+	for _, question := range allQuestions {
+		options := []string{}
+		scalelabel := model.ScaleLabels{}
+		validation := model.Validations{}
+		switch question.Type {
+		case "MultipleChoice", "Checkbox", "Dropdown":
+			var ok bool
+			options, ok = optionMap[question.ID]
+			if !ok {
+				options = []string{}
+			}
+		case "LinearScale":
+			var ok bool
+			scalelabel, ok = scaleLabelMap[question.ID]
+			if !ok {
+				scalelabel = model.ScaleLabels{}
+			}
+		case "Text", "Number":
+			var ok bool
+			validation, ok = validationMap[question.ID]
+			if !ok {
+				validation = model.Validations{}
+			}
+		}
+
+		outputs = append(outputs, output.QuestionInfo{
+			QuestionID:      question.ID,
+			QuestionType:    question.Type,
+			QuestionNum:     question.QuestionNum,
+			PageNum:         question.PageNum,
+			Body:            question.Body,
+			IsRequired:      question.IsRequired,
+			CreatedAt:       question.CreatedAt.Format(time.RFC3339),
+			Options:         options,
+			ScaleLabelRight: scalelabel.ScaleLabelRight,
+			ScaleLabelLeft:  scalelabel.ScaleLabelLeft,
+			ScaleMin:        scalelabel.ScaleMin,
+			ScaleMax:        scalelabel.ScaleMax,
+			RegexPattern:    validation.RegexPattern,
+			MinBound:        validation.MinBound,
+			MaxBound:        validation.MaxBound,
+		})
+	}
+
+	return outputs, nil
 
 }
