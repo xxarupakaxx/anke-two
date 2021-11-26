7a6019117a0119115175687ac13c0623ff6f0a10
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-26 22:37:12 +0900

    input responses

--- usecase/input/responses.go
+++ usecase/input/responses.go
@@ -4,7 +4,23 @@ import "github.com/xxarupkaxx/anke-two/domain/model"
 
 // Responses 質問に対する回答一覧の構造体
 type Responses struct {
+	UserID      string
 	ID          int                  `json:"questionnaireID" validate:"min=0"`
 	Temporarily bool                 `json:"temporarily"`
 	Body        []model.ResponseBody `json:"body" validate:"required,dive"`
+}
+
+type GetResponse struct {
+	ResponseID int
+}
+
+type EditResponse struct {
+	ResponseID  int
+	ID          int                  `json:"questionnaireID" validate:"min=0"`
+	Temporarily bool                 `json:"temporarily"`
+	Body        []model.ResponseBody `json:"body" validate:"required,dive"`
+}
+
+type DeleteResponse struct {
+	ResponseID int
 }
