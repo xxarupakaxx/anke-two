cc1ee65ad5caf34c31f6d184459ef88de4168e8b
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-26 21:00:42 +0900

    UserUsecaseのインターフェイス化

---
+++ usecase/users.go
@@ -0,0 +1,53 @@
+package usecase
+
+import (
+	"context"
+	"github.com/xxarupkaxx/anke-two/domain/model"
+	"github.com/xxarupkaxx/anke-two/domain/repository"
+	"github.com/xxarupkaxx/anke-two/usecase/input"
+	"github.com/xxarupkaxx/anke-two/usecase/output"
+)
+
+type user struct {
+	repository.IRespondent
+	repository.IQuestionnaire
+	repository.ITarget
+	repository.IAdministrator
+}
+
+func newUser(IRespondent repository.IRespondent, IQuestionnaire repository.IQuestionnaire, ITarget repository.ITarget, IAdministrator repository.IAdministrator) UsersUsecase {
+	return &user{IRespondent: IRespondent, IQuestionnaire: IQuestionnaire, ITarget: ITarget, IAdministrator: IAdministrator}
+}
+
+func (u *user) GetUsersMe(ctx context.Context, me input.GetMe) (output.GetMe, error) {
+	panic("implement me")
+}
+
+func (u *user) GetMyResponses(ctx context.Context, me input.GetMe) ([]model.RespondentInfo, error) {
+	panic("implement me")
+}
+
+func (u *user) GetMyResponsesByID(ctx context.Context, response input.GetMyResponse) ([]model.RespondentInfo, error) {
+	panic("implement me")
+}
+
+func (u *user) GetTargetedQuestionnaire(ctx context.Context, request input.GetTargetedQuestionnaire) ([]model.TargetedQuestionnaire, error) {
+	panic("implement me")
+}
+
+func (u *user) GetMyQuestionnaire(ctx context.Context, me input.GetMe) ([]output.QuestionnaireInfo, error) {
+	panic("implement me")
+}
+
+func (u *user) GetTargetedQuestionnairesByID(ctx context.Context, qid input.GetTargetsByTraQID) ([]model.TargetedQuestionnaire, error) {
+	panic("implement me")
+}
+
+type UsersUsecase interface {
+	GetUsersMe(ctx context.Context, me input.GetMe) (output.GetMe, error)
+	GetMyResponses(ctx context.Context, me input.GetMe) ([]model.RespondentInfo, error)
+	GetMyResponsesByID(ctx context.Context, response input.GetMyResponse) ([]model.RespondentInfo, error)
+	GetTargetedQuestionnaire(ctx context.Context, request input.GetTargetedQuestionnaire) ([]model.TargetedQuestionnaire, error)
+	GetMyQuestionnaire(ctx context.Context, me input.GetMe) ([]output.QuestionnaireInfo, error)
+	GetTargetedQuestionnairesByID(ctx context.Context, qid input.GetTargetsByTraQID) ([]model.TargetedQuestionnaire, error)
+}
