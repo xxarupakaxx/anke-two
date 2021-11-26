6d6dee392ecf1b4981492b5aef41da26ff6b9ff5
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-27 02:56:17 +0900

    EditQuestionの実装

--- interfaces/router/questions.go
+++ interfaces/router/questions.go
@@ -3,6 +3,9 @@ package router
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/xxarupkaxx/anke-two/usecase"
+	"github.com/xxarupkaxx/anke-two/usecase/input"
+	"net/http"
+	"strconv"
 )
 
 type question struct {
@@ -14,7 +17,38 @@ func NewQuestionAPI(questionUsecase usecase.QuestionUsecase) QuestionAPI {
 }
 
 func (q *question) EditQuestion(c echo.Context) error {
-	panic("implement me")
+	questionID, err := strconv.Atoi(c.Param("questionID"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest)
+	}
+
+	in := input.EditQuestionRequest{}
+	in.QuestionID = questionID
+
+	if err := c.Bind(&in); err != nil {
+		c.Logger().Error(err)
+		return echo.NewHTTPError(http.StatusBadRequest)
+	}
+
+	statusCode, err := usecase.ValidateRequest(c, in)
+	if err != nil {
+		switch statusCode {
+		case http.StatusBadRequest:
+			c.Logger().Info(err)
+			return echo.NewHTTPError(statusCode)
+		case http.StatusInternalServerError:
+			c.Logger().Error(err)
+			return echo.NewHTTPError(statusCode)
+		}
+	}
+
+	err = q.QuestionUsecase.EditQuestion(c.Request().Context(), in)
+	if err != nil {
+		c.Logger().Error(err)
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	return c.NoContent(http.StatusOK)
 }
 
 func (q *question) DeleteQuestion(c echo.Context) error {
