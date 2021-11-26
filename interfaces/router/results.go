0b0289a56a7d127aa13ccdb05cd9ddedf925a2c5
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-27 02:22:56 +0900

    GetResultの実装

--- interfaces/router/results.go
+++ interfaces/router/results.go
@@ -3,6 +3,9 @@ package router
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/xxarupkaxx/anke-two/usecase"
+	"github.com/xxarupkaxx/anke-two/usecase/input"
+	"net/http"
+	"strconv"
 )
 
 type result struct {
@@ -14,7 +17,24 @@ func NewResultAPI(resultUsecase usecase.ResultUsecase) ResultAPI {
 }
 
 func (r *result) GetResults(c echo.Context) error {
-	panic("implement me")
+	sort := c.QueryParam("sort")
+	questionnaireID, err := strconv.Atoi(c.Param("questionnaireID"))
+	if err != nil {
+		c.Logger().Info(err)
+		return echo.NewHTTPError(http.StatusBadRequest)
+	}
+
+	in := input.GetResults{
+		Sort:            sort,
+		QuestionnaireID: questionnaireID,
+	}
+	out, err := r.ResultUsecase.GetResults(c.Request().Context(), in)
+	if err != nil {
+		c.Logger().Error(err)
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, out)
 }
 
 type ResultAPI interface {
