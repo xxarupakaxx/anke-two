ac44f701b8a578176b7ab6f3297a775b958df50d
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-26 22:30:21 +0900

    GetMyQuestionnaire

--- usecase/users.go
+++ usecase/users.go
@@ -6,6 +6,7 @@ import (
 	"github.com/xxarupkaxx/anke-two/domain/repository"
 	"github.com/xxarupkaxx/anke-two/usecase/input"
 	"github.com/xxarupkaxx/anke-two/usecase/output"
+	"time"
 )
 
 type user struct {
@@ -52,7 +53,110 @@ func (u *user) GetTargetedQuestionnaire(ctx context.Context, request input.GetTa
 }
 
 func (u *user) GetMyQuestionnaire(ctx context.Context, me input.GetMe) ([]output.QuestionnaireInfo, error) {
-	panic("implement me")
+	questionnaires, err := u.IQuestionnaire.GetAdminQuestionnaires(ctx, me.UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	questionnaireIDs := make([]int, 0, len(questionnaires))
+	for _, q := range questionnaires {
+		questionnaireIDs = append(questionnaireIDs, q.ID)
+	}
+
+	targets, err := u.ITarget.GetTargets(ctx, questionnaireIDs)
+	if err != nil {
+		return nil, err
+	}
+
+	targetMap := map[int][]string{}
+	for _, t := range targets {
+		target, ok := targetMap[t.QuestionnaireID]
+		if !ok {
+			targetMap[t.QuestionnaireID] = []string{t.UserTraqid}
+		} else {
+			targetMap[t.QuestionnaireID] = append(target, t.UserTraqid)
+		}
+	}
+
+	respondents, err := u.IRespondent.GetRespondentsUserIDs(ctx, questionnaireIDs)
+	if err != nil {
+		return nil, err
+	}
+
+	respondentMap := map[int][]string{}
+	for _, respondent := range respondents {
+		rspdts, ok := respondentMap[respondent.QuestionnaireID]
+		if !ok {
+			respondentMap[respondent.QuestionnaireID] = []string{respondent.UserTraqid}
+		} else {
+			respondentMap[respondent.QuestionnaireID] = append(rspdts, respondent.UserTraqid)
+		}
+	}
+
+	administrators, err := u.IAdministrator.GetAdministrators(ctx, questionnaireIDs)
+	if err != nil {
+		return nil, err
+	}
+
+	administratorMap := map[int][]string{}
+	for _, administrator := range administrators {
+		admins, ok := administratorMap[administrator.QuestionnaireID]
+		if !ok {
+			administratorMap[administrator.QuestionnaireID] = []string{administrator.UserTraqid}
+		} else {
+			administratorMap[administrator.QuestionnaireID] = append(admins, administrator.UserTraqid)
+		}
+	}
+
+	op := []output.QuestionnaireInfo{}
+
+	for _, q := range questionnaires {
+		targets, ok := targetMap[q.ID]
+		if !ok {
+			targets = []string{}
+		}
+
+		administrators, ok := administratorMap[q.ID]
+		if !ok {
+			administrators = []string{}
+		}
+
+		respondents, ok := respondentMap[q.ID]
+		if !ok {
+			respondents = []string{}
+		}
+
+		allresponded := true
+		for _, t := range targets {
+			found := false
+			for _, r := range respondents {
+				if t == r {
+					found = true
+					break
+				}
+			}
+			if !found {
+				allresponded = false
+				break
+			}
+		}
+
+		op = append(op, output.QuestionnaireInfo{
+			ID:             q.ID,
+			Title:          q.Title,
+			Description:    q.Description,
+			ResTimeLimit:   q.ResTimeLimit,
+			CreatedAt:      q.CreatedAt.Format(time.RFC3339),
+			ModifiedAt:     q.ModifiedAt.Format(time.RFC3339),
+			ResSharedTo:    q.ResSharedTo,
+			AllResponded:   allresponded,
+			Targets:        targets,
+			Administrators: administrators,
+			Respondents:    respondents,
+		})
+	}
+
+	return op, nil
 }
 
 func (u *user) GetTargetedQuestionnairesByID(ctx context.Context, qid input.GetTargetsByTraQID) ([]model.TargetedQuestionnaire, error) {
