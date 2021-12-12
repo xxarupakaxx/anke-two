3f01fe462f2e29278bc48f5e789dce8c7e79463b
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-13 00:22:19 +0900

    Optionsの説明

--- src/model/options.go
+++ src/model/options.go
@@ -1,6 +1,6 @@
 package model
 
-
+// Options 選択肢のテーブル
 type Options struct {
 	ID         int    `gorm:"type:int(11);AUTO_INCREMENT;not null;primaryKey"`
 	QuestionID int    `gorm:"type:int(11);not null"`
