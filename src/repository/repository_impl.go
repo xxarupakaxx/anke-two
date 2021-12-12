bc94b5e33e7312438905c0718b999ed05b71bb5f
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-13 01:43:08 +0900

    DBとの接続を書いた

---
+++ src/repository/repository_impl.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"fmt"
+	"github.com/xxarupkaxx/anke-two/config"
+	"github.com/xxarupkaxx/anke-two/src/model"
+	"gorm.io/gorm"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm/logger"
+)
+
+var allTables =[]interface{}{
+	model.Question{},
+	model.QuestionType{},
+	model.Questionnaire{},
+	model.ResSharedTo{},
+	model.Response{},
+	model.Respondent{},
+	model.Options{},
+	model.Validation{},
+	model.Respondent{},
+	model.Target{},
+	model.Administrator{},
+}
+
+type GormRepository struct {
+	db *gorm.DB
+}
+
+func connectDB(c *config.Config) (*gorm.DB,error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", c.MariaDBUsername, c.MariaDBPassword, c.MariaDBHostname, c.MariaDBDatabase) + "?parseTime=true&loc=Local&charset=utf8mb4"
+	logLevel := logger.Info
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logLevel)})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect DB : %w", err)
+	}
+
+	db = db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci")
+
+	err = db.AutoMigrate(allTables...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to migrate: %w", err)
+	}
+
+	return db, nil
+}
+
+
