5576f5228db6c7325799077a4204dfd9ca63c937
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-13 00:38:48 +0900

    QustionTypeテーブルの追加

--- src/model/question.go
+++ src/model/question.go
@@ -17,8 +17,16 @@ type Question struct {
 	CreatedAt       time.Time      `gorm:"precision:6"`
 	UpdatedAt       time.Time      `gorm:"precision:6"`
 
-	Options         []Options     `json:"-" gorm:"foreignKey:QuestionID"`
-	Responses       []Response   `json:"-"  gorm:"foreignKey:QuestionID"`
-	ScaleLabels     []ScaleLabel `json:"-"  gorm:"foreignKey:QuestionID"`
-	Validations     []Validation `json:"-"  gorm:"foreignKey:QuestionID"`
+	QuestionType QuestionType       `gorm:"foreignKey:ID;references:Type"`
+	Options     []Options    		`gorm:"foreignKey:QuestionID"`
+	Responses   []Response          `gorm:"foreignKey:QuestionID"`
+	ScaleLabels []ScaleLabel        `gorm:"foreignKey:QuestionID"`
+	Validations []Validation        `gorm:"foreignKey:QuestionID"`
+}
+
+// QuestionType 質問の種類。 'Text'、'TextArea'、'Number'、'MultipleChoice'、'Checkbox', 'Dropdown', 'LinearScale', 'Date', 'Time' のテーブル
+type QuestionType struct {
+	ID     int    `gorm:"type:int(11) AUTO_INCREMENT;not null;primaryKey"`
+	Name   string `gorm:"type:varchar(32);not null"`
+	Active bool   `gorm:"type:boolean;not null;default:true"`
 }
