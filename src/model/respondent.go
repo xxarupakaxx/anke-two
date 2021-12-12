6a9afe009cb5d20e6aabb523263d912571af8e68
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-13 00:33:09 +0900

    respondentテーブルの追加

---
+++ src/model/respondent.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"gopkg.in/guregu/null.v4"
+	"gorm.io/gorm"
+	"time"
+)
+
+// Respondents アンケートごとの回答者
+type Respondents struct {
+	ResponseID      int            `gorm:"column:response_id;type:int(11) AUTO_INCREMENT;not null;primaryKey"`
+	QuestionnaireID int            `gorm:"type:int(11);not null"`
+	UserTraqid      string         `gorm:"type:varchar(32);size:32;default:NULL"`
+	UpdatedAt       time.Time      `gorm:"precision:6"`
+	SubmittedAt     null.Time      `gorm:"precision:6"`
+	DeletedAt       gorm.DeletedAt `gorm:"precision:6"`
+
+	Responses       []Response     `gorm:"foreignKey:ResponseID;references:ResponseID"`
+}
