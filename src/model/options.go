d9d3c5877da71ee855d58887ffd16453865ec540
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-13 12:04:48 +0900

    fix

--- src/model/options.go
+++ src/model/options.go
@@ -5,7 +5,7 @@ type Option struct {
 	ID         int    `gorm:"type:int(11);AUTO_INCREMENT;not null;primaryKey"`
 	QuestionID int    `gorm:"type:int(11);not null"`
 	OptionNum  int    `gorm:"type:int(11);not null"`
-	Body       string `gorm:"type:text;default:NULL;"`
+	Body       string `gorm:"type:text;default:NULL"`
 }
 
 func (o *Option) TableName() string {
