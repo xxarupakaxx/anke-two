28d3a93427f7769acdc381bd81c82c17ad8b35b4
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-14 12:04:55 +0900

    Optionの定義

---
+++ src/repository/option.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"context"
+	"github.com/xxarupkaxx/anke-two/src/model"
+)
+
+type Option interface {
+	GetOptions(ctx context.Context, questionIDs []int) ([]*model.Option, error)
+	CreateOption(ctx context.Context, option *model.Option) error
+	DeleteOption(Ctx context.Context, questionID int) error
+	UpdateOption(ctx context.Context, option *model.Option) error
+}
