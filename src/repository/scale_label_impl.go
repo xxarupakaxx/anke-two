0ea3bde10c9b873912baac96c8583b856d6c7afd
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-15 08:40:58 +0900

    UpdateScaleLabelの実装

---
+++ src/repository/scale_label_impl.go
@@ -0,0 +1,71 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/xxarupkaxx/anke-two/src/model"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+func (repo *GormRepository) GetScaleLabel(ctx context.Context, questionID int) (*model.ScaleLabel, error) {
+	panic("implement me")
+}
+
+func (repo *GormRepository) CreateScaleLabel(ctx context.Context, label *model.ScaleLabel) error {
+	panic("implement me")
+}
+
+func (repo *GormRepository) UpdateScaleLabel(ctx context.Context, label *model.ScaleLabel) error {
+	db, err := repo.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db:%w", err)
+	}
+	var preScale *model.ScaleLabel
+
+	err = db.
+		Where("question_id", label.QuestionID).
+		Clauses(clause.Locking{Strength: "UPDATE"}).
+		Find(&preScale).Error
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return fmt.Errorf("failed get to preScaleLable :%w", err)
+	}
+
+	mapScale := compareChangeField(preScale, label)
+	result := db.
+		Session(&gorm.Session{}).
+		Where("question_id = ?", label.QuestionID).
+		Updates(&mapScale)
+	err = result.Error
+	if err != nil {
+		return fmt.Errorf("failed to update scaleLabel :%w", err)
+	}
+	if result.RowsAffected == 0 {
+		return ErrNoRecordUpdated
+	}
+
+	return nil
+}
+
+func (repo *GormRepository) DeleteScaleLabel(ctx context.Context, questionID int) error {
+	panic("implement me")
+}
+
+func compareChangeField(preScale, scale *model.ScaleLabel) map[string]interface{} {
+	mapScale := map[string]interface{}{}
+	if preScale.ScaleLabelLeft != scale.ScaleLabelLeft {
+		mapScale["scale_label_left"] = scale.ScaleLabelLeft
+	}
+	if preScale.ScaleLabelRight != scale.ScaleLabelRight {
+		mapScale["scale_label_right"] = scale.ScaleLabelRight
+	}
+	if preScale.ScaleMax != scale.ScaleMax {
+		mapScale["scale_max"] = scale.ScaleMin
+	}
+	if preScale.ScaleMin != scale.ScaleMin {
+		mapScale["scale_min"] = scale.ScaleMin
+	}
+
+	return mapScale
+}
