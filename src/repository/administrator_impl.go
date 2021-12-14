8b7fe6b809f082629f9adaf3efc67178f283099a
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-14 11:14:33 +0900

    CreateAdminsの実装

--- src/repository/administrator_impl.go
+++ src/repository/administrator_impl.go
@@ -2,11 +2,36 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"github.com/xxarupkaxx/anke-two/src/model"
 )
 
 func (repo *GormRepository) CreateAdmins(ctx context.Context, questionnaireID int, administrator []string) error {
-	panic("implement me")
+	if questionnaireID < 0 {
+		return ErrNotFormat
+	}
+	if len(administrator) == 0 {
+		return nil
+	}
+
+	db, err := repo.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db:%w", err)
+	}
+
+	admins := make([]*model.Administrator, len(administrator))
+	for _, a := range administrator {
+		admins = append(admins, &model.Administrator{
+			QuestionnaireID: questionnaireID,
+			TraqID:          a,
+		})
+	}
+	err = db.Create(&admins).Error
+	if err != nil {
+		return fmt.Errorf("failed to create administrators :%w", err)
+	}
+
+	return nil
 }
 
 func (repo *GormRepository) GetAdmins(ctx context.Context, questionnaireIDs []int) ([]*model.Administrator, error) {
@@ -16,4 +41,3 @@ func (repo *GormRepository) GetAdmins(ctx context.Context, questionnaireIDs []in
 func (repo *GormRepository) DeleteAdmin(ctx context.Context, questionnaireID int) {
 	panic("implement me")
 }
-
