4608f01b4a6c299ecd828e7f68c47cd2c6672979
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-16 03:41:16 +0900

    dbの取得

--- src/repository/validation_impl.go
+++ src/repository/validation_impl.go
@@ -2,22 +2,35 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"github.com/xxarupkaxx/anke-two/src/model"
 )
 
 func (repo *GormRepository) GetValidation(ctx context.Context, questionID int) (*model.Validation, error) {
-	panic("implement me")
+	db, err := repo.getDB(ctx)
+	if err != nil {
+		return nil,fmt.Errorf("failed to get db:%w", err)
+	}
 }
 
 func (repo *GormRepository) CreateValidation(ctx context.Context, validation *model.Validation) error {
-	panic("implement me")
+	db, err := repo.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db:%w", err)
+	}
 }
 
 func (repo *GormRepository) DeleteValidation(ctx context.Context, questionID int) error {
-	panic("implement me")
+	db, err := repo.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db:%w", err)
+	}
 }
 
 func (repo *GormRepository) UpdateValidation(ctx context.Context, validation *model.Validation) error {
-	panic("implement me")
+	db, err := repo.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db:%w", err)
+	}
 }
 
