578a802fd576d6b8cce96a9474c104621c2ba658
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-15 23:37:32 +0900

    GetRespondentの実装

---
+++ src/repository/respodent_impl.go
@@ -0,0 +1,40 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"github.com/xxarupkaxx/anke-two/src/model"
+)
+
+func (repo *GormRepository) GetRespondent(ctx context.Context, questionnaireID int) ([]*model.Respondent, error) {
+	db, err := repo.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db:%w", err)
+	}
+
+	respondents := make([]*model.Respondent, 0)
+	err = db.
+		Where("questionnaire_id = ?", questionnaireID).
+		Find(&respondents).Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to get respondents :%w", err)
+	}
+
+	return respondents, nil
+}
+
+func (repo *GormRepository) GetMyRespondent(ctx context.Context, questionnaireID int, traPID string) ([]*model.Respondent, error) {
+	panic("implement me")
+}
+
+func (repo *GormRepository) CreateRespondent(ctx context.Context, respondent *model.Respondent) (int, error) {
+	panic("implement me")
+}
+
+func (repo *GormRepository) DeleteRespondent(ctx context.Context, responseID int) error {
+	panic("implement me")
+}
+
+func (repo *GormRepository) UpdateRespondent(ctx context.Context, respondent *model.Respondent) error {
+	panic("implement me")
+}
