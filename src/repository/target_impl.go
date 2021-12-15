46e80930a4142b1944ab943773690229c2ec2bbe
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-15 19:28:21 +0900

    GetTargetsの実装

--- src/repository/target_impl.go
+++ src/repository/target_impl.go
@@ -2,11 +2,26 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"github.com/xxarupkaxx/anke-two/src/model"
 )
 
 func (repo *GormRepository) GetTargets(ctx context.Context, questionnaireID int) ([]*model.Target, error) {
-	panic("implement me")
+	db, err := repo.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db:%w", err)
+	}
+
+	targets := make([]*model.Target, 0)
+
+	err = db.
+		Where("questionnaire_id = ?", questionnaireID).
+		Find(&targets).Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to get targets :%w", err)
+	}
+
+	return targets, nil
 }
 
 func (repo *GormRepository) CreateTargets(ctx context.Context, questionnaireID int, targets []string) error {
@@ -16,4 +31,3 @@ func (repo *GormRepository) CreateTargets(ctx context.Context, questionnaireID i
 func (repo *GormRepository) DeleteTargets(ctx context.Context, questionnaireID int) error {
 	panic("implement me")
 }
-
