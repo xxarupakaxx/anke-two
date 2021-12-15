daded23c808d75519920d068813997f4d7009514
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-15 23:40:59 +0900

    GetMyRespondentの実装

--- src/repository/respodent_impl.go
+++ src/repository/respodent_impl.go
@@ -24,7 +24,21 @@ func (repo *GormRepository) GetRespondent(ctx context.Context, questionnaireID i
 }
 
 func (repo *GormRepository) GetMyRespondent(ctx context.Context, questionnaireID int, traPID string) ([]*model.Respondent, error) {
-	panic("implement me")
+	db, err := repo.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db:%w", err)
+	}
+
+	respondents := make([]*model.Respondent, 0)
+
+	err = db.
+		Where("questionnaire_id = ? AND user_traqid = ?", questionnaireID, traPID).
+		Find(&respondents).Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to get respondents :%w", err)
+	}
+
+	return respondents, nil
 }
 
 func (repo *GormRepository) CreateRespondent(ctx context.Context, respondent *model.Respondent) (int, error) {
